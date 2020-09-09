487296d642f1a56f46475bf6aabd10d96ea7f3c6
shuvo <khairul.shuvo27@gmail.com>
2020-09-08 23:21:21 -0400

    database & Mysql connected

--- Generic/src/main/java/common/WebAPI.java
+++ Generic/src/main/java/common/WebAPI.java
@@ -140,7 +140,7 @@ public class WebAPI {
 //                                  String browserVersion, @Optional("https://www.united.com/en/us")
 //                                  String browserVersion, @Optional("https://www.geico.com/")
 //                                  String browserVersion, @Optional("https://www.tripadvisor.com//")
-                                  String browserVersion, @Optional("https://www.target.com") String url) throws IOException {
+                                  String browserVersion, @Optional("https://www.google.com") String url) throws IOException {
 
         if (useCloudEnv == true) {
             if (cloudEnvName.equalsIgnoreCase("browserstack")) {
