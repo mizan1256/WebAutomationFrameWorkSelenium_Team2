f8a07c7a88e8997346b330df401603a8d5273b75
zakerin <shahzakerin@Shahs-MacBook-Pro.local>
2020-09-26 13:54:00 -0400

    Final Update

---
+++ TargetEcommerce/src/test/java/testhome/TargetEcommerceHomeTest.java
@@ -0,0 +1,69 @@
+package testhome;
+
+import common.WebAPI;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import targetHome.TargetHome;
+
+public class TargetEcommerceHomeTest extends WebAPI {
+    TargetHome targetHome;
+    @BeforeMethod
+    public void getInit(){
+        targetHome= PageFactory.initElements(driver,TargetHome.class);
+    }
+    @Test
+    public void testSearchBoxIsDisplayed() throws InterruptedException {
+        targetHome.searchBoxIsDisplayed();
+        targetHome.validateSearchBoxDisplayed();
+    }
+    @Test
+    public void testSearchBoxCheck() throws InterruptedException {
+        targetHome.searchBoxCheck("samsung");
+        targetHome.validateSearchText();
+    }
+    @Test
+    public void testSearchNewArrivals() throws InterruptedException {
+        targetHome.searchBoxCheck("new arrivals");
+        targetHome.validateSearchNewArrival();
+    }
+    @Test
+    public void testSearchCartDisplayed() throws InterruptedException {
+        targetHome.searchCartIsDisplayed();
+        targetHome.validateSearchBoxDisplayed();
+    }
+    @Test
+    public void testGetHomePageLogo() throws InterruptedException {
+        targetHome.getHomePageLogo();
+        targetHome.validateGetHomePageLogo();
+    }
+    @Test
+    public void testClickCollegeButton() throws InterruptedException {
+        targetHome.clickCollegeButton();
+        targetHome.validateClickCollegeButton();
+    }
+    @Test
+    public void testClickMensButton() throws InterruptedException {
+        targetHome.clickMensButton();
+        targetHome.validateClickMensButton();
+    }
+    @Test
+    public void testClickSameDayDelivery() throws InterruptedException {
+        targetHome.clickSameDayDelivery();
+        targetHome.validateClickSameDayDelivery();
+    }
+    @Test
+    public void testTargetSearchBoxCheckGetItemValue() throws InterruptedException {
+        targetHome.targetSearchBoxCheckGetItemValue();
+
+    }
+    @Test
+    public void testTargetSearchBoxCheckGetItemsListFromExcel() throws Exception {
+        targetHome.targetSearchBoxCheckGetItemsListFromExcel();
+    }
+    @Test
+    public void testTargetSearchBoxCheckGetItemsListFromDB() throws Exception {
+        targetHome.targetSearchBoxCheckGetItemsListFromDB();
+    }
+
+}
